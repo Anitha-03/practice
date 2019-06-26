dc2d2587b27eb184b59275f7450e4a67e013bfcb
Lipsa <lmishra@yahoo.com>
2019-06-26 14:51:20 +0530

    Lipsa added a file

---
+++ javanew2.java
@@ -0,0 +1,8 @@
+class javanew2
+{
+public static void main(string[] args)
+{
+  System.out.println("Hello this is lipsa");
+  System.out.println("Hello this is lipsa and i am creating a java file");
+}
+}
